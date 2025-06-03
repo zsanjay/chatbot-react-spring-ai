2a7042e648137d23c998f76bad000ebc22a124c5
Sanjay Mehta <sanjay.mehta@hidubai.com>
2025-06-03 20:44:50 +0400

    removed unused dependency and imports

--- backend/chatbot/src/main/java/com/zsanjay/chatbot/service/impl/ImageStorageService.java
+++ backend/chatbot/src/main/java/com/zsanjay/chatbot/service/impl/ImageStorageService.java
@@ -1,12 +1,10 @@
 package com.zsanjay.chatbot.service.impl;
 
-import com.amazonaws.services.s3.AmazonS3;
 import com.zsanjay.chatbot.entity.User;
 import com.zsanjay.chatbot.exception.UserNotFoundException;
 import com.zsanjay.chatbot.repository.UserRepository;
 import com.zsanjay.chatbot.service.StorageService;
 import lombok.SneakyThrows;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
